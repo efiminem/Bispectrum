8d6b5fe1bbe39fdfaa92f90f1f6a17266dc197d7
efiminem <efimmazhnik@gmail.com>
2017-03-27 11:54:24 +0300

    complex structure

--- ultraspherical.cpp
+++ ultraspherical.cpp
@@ -7,6 +7,10 @@
 int PT(int l, int m) {return m+(l*(l+1)/2);}
 int YR(int l, int m) {return m+l+(l*l);}
 
+struct complex {
+double r, i;
+};
+
 class Hardcore {
 private:
     std::vector<double> A_v = std::vector<double>(100,0);
@@ -42,21 +46,22 @@ public:
 class ULTRAS {
 private:
     Hardcore tab; //table of already calculated factors
-    std::vector<double> P_v;
-    std::vector<double> Y_v;
-    std::vector<double> Theta;
+    std::vector<double> P_v; //vector of ASF
+    std::vector<double> Y_v_r; //real spherical part
+    std::vector<double> Y_v_i; //imaginary spherical part
+    std::vector<double> Theta; // vector of angles
     void printException (std::string message) {
         std::cout << message;
     }
     void computeALF (std::size_t L, double x) {
         const double sintheta = sqrt(1.-x*x);
         double temp = tab.temp;
-        if (PT(L, L) >= 100) {
+        /*if (PT(L, L) >= 100) {
             for (int i=0; i<PT(L, L)- 100; i++) {
                 P_v.push_back(0);
-                Y_v.push_back(0);
+                Y_v_r.push_back(0);
             }
-        }
+        }*/
         P_v[PT(0,0)] = temp;
         if (L > 0) {
             P_v[PT(1,0)] = x*tab.SQRT3*temp;
@@ -74,7 +79,7 @@ P_v[PT(l-2,m)]);
         }
     }
     void computeY (const std::size_t L, double y) {
-        for (std::size_t l=0; l <= L; l++) Y_v[YR(l,0)] = 
+        for (std::size_t l=0; l <= L; l++) Y_v_r[YR(l,0)] = 
 P_v[PT(l,0)]*0.5*tab.SQRT2;
         double c1 = 1.0,c2 = y;
         double s1 = 0.0,s2 = -sqrt(1.-y*y);
@@ -84,20 +89,28 @@ P_v[PT(l,0)]*0.5*tab.SQRT2;
             double c = tc*c1-c2;
             s2 = s1; s1 = s; c2 = c1; c1 = c;
             for (std::size_t l = m; l <= L; l++) {
-                Y_v[YR(l,-m)]=P_v[PT(l,m)]*s;
-                Y_v[YR(l,m)]=P_v[PT(l,m)]*c;
+                Y_v_r[YR(l,m)]=P_v[PT(l,m)]*c;
+                Y_v_r[YR(l,-m)]=-Y_v_r[YR(l,m)];
+                Y_v_i[YR(l,m)]=-P_v[PT(l,m)]*s;
+                Y_v_i[YR(l,-m)]=Y_v_i[YR(l,m)];
             }
         }
     }
 public:
     void calculate(std::size_t maxL) {
     P_v = std::vector<double>(PT(maxL, maxL)+1,0);
-    Y_v = std::vector<double>(YR(maxL, maxL)+1,0);
+    Y_v_r = std::vector<double>(YR(maxL, maxL)+1,0);
+    Y_v_i = std::vector<double>(YR(maxL, maxL)+1,0);
     tab.update(maxL);
     computeALF(maxL, Theta[0]);
     computeY(maxL, Theta[1]);
     }
-    double Y (int l, int m) {return Y_v[YR(l,m)];}
+    complex Y (int l, int m) {
+        complex usphFunction;
+        usphFunction.r = Y_v_r[YR(l,m)];
+        usphFunction.i = Y_v_i[YR(l,m)];
+        return usphFunction;
+        }
     template<typename T = double> void ULTRAS_REST(T t) {
     Theta.push_back(t);
     //end of loop
@@ -125,7 +138,7 @@ int main() {
 ULTRAS vec(0.5, 0.5, 0.5);
 //TODO: what's wrong with 10000 ?
 vec.calculate(1000);
-std::cout << vec.Y(10, 3);
+std::cout << vec.Y(10, -5).i;
 return 0;
 }
 
