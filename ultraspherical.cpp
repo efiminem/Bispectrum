f228b9fae427a4a7d53642cc3818c71bd47e41df
efiminem <efimmazhnik@gmail.com>
2017-05-18 05:56:04 +0300

    j is integer now

--- ultraspherical.cpp
+++ ultraspherical.cpp
@@ -16,10 +16,13 @@ class ULTRAS {
 private:
     std::size_t numofArguments;
     std::size_t numofIndices;
-    std::vector<double> Theta; // vector of angles
+    std::vector<double> Theta;
     std::vector<int> Index;
 
-    std::vector<double> CalculateAnotherMultiplier (double j, const std::size_t maxL, double sintheta) {
+    double GetNullValue (int j) {
+    
+    }
+    std::vector<double> CalculateAnotherMultiplier (int j, const std::size_t maxL, double sintheta) {
         std::vector<double> multiplier;
         double costheta = sqrt(1.-sintheta*sintheta);
         double temp=0;
@@ -28,24 +31,23 @@ private:
         if (j==3) temp=sqrt(2/PI); //TODO Null Value
         multiplier[PT(0,0)] = temp;
         if (maxL > 0) {
-            multiplier[PT(1,0)] = costheta*sqrt(j+1)*temp; //TODO: get it from Hardcode
-            temp = -sqrt((j+1)/j) * sintheta * temp;
+            multiplier[PT(1,0)] = costheta*sqrt(j+1)*temp; //TODO: Get it from Hardcode
+            temp = -sqrt((j+1.)/j) * sintheta * temp;
             multiplier[PT(1,1)] = temp;
             for (std::size_t l=2; l <= maxL ; l++) {
                 for (std::size_t m =0; m <l-1; m++) {
-                double A = sqrt((2*l+j-3)*(2*l + j -1)/((l-m)*(l+m+j-2)));
-                double B = -sqrt((l+m+j-3)*(l-m-1)/((2*l+j-3)*(2*l+j-5)));
+                double A = sqrt((2*l+j-3.)*(2*l + j -1.)/((l-m)*(l+m+j-2.)));
+                double B = -sqrt((l+m+j-3.)*(l-m-1)/((2*l+j-3.)*(2*l+j-5.)));
                 multiplier[PT(l,m)]=A*(costheta*multiplier[PT(l-1,m)]+B* multiplier[PT(l-2,m)]);
                 }
             multiplier[PT(l,l-1)] = costheta*sqrt(2*(l-1)+j+1)*temp;
-            temp=-sqrt((2*l + j + 1)/(2*l + j - 2))*sintheta*temp;
+            temp=-sqrt((2*l + j + 1.)/(2*l + j - 2.))*sintheta*temp;
             multiplier[PT(l,l)]=temp;
             }
          }
          return multiplier;
     }
     std::vector<complex> CalculateFirstMultiplier(const std::size_t maxL, double sintheta) {
-        //for (std::size_t l=0; l <= maxL; l++) Y_v_r[YR(l,0)] =P_v[PT(l,0)];
         std::vector<complex> multiplier;
         complex normalization_factor = {1/sqrt(2*PI), 1/sqrt(2*PI)};
         for (std::size_t l=0; l<= maxL; l++) multiplier.push_back(normalization_factor);
@@ -69,7 +71,7 @@ public:
     void calculate(std::size_t maxL) {
        multiplier = CalculateFirstMultiplier(maxL, Theta[0]); //j=1 complex number
        for (int j=2; j<=numofArguments; j++) {
-          multipliers.push_back(CalculateAnotherMultiplier(j, maxL, Theta[j-1])); //j!=1 real number
+          multipliers.push_back(CalculateAnotherMultiplier(j, maxL, Theta[j-1])); //j!=1 real numbers
        }
 
     }
@@ -112,10 +114,8 @@ public:
 
 int main() {
 ULTRAS vec(0.707, 0.707, 0.707); //sintheta
-//TODO: what's wrong with 10000 ?
-vec.calculate(100);
-std::cout << vec.Y(1, 1, 1).r << '\n';
-//std::cout << vec.multiplier[1].r << " * " << vec.multipliers[0][PT(1,1)] << " * " << vec.multipliers[1][PT(1,1)];
+vec.calculate(1000);
+std::cout << vec.Y(0, 1, 2).r << '\n';
 return 0;
 }
 
