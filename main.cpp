b74ef369a616a483341fb97cb5a600e2e6b0f840
efiminem <efimmazhnik@gmail.com>
2017-05-26 16:39:20 +0300

    angles comment

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include "ultraspherical.h"
 
 int main() {
-ULTRAS vec(4); //dimensions
-vec.set({0.5, 0.5, 0.5});
+ULTRAS vec(4);
+vec.set({0.5, 0.5, 0.5}); //sintheta
 vec.pack(100);
 std::cout << vec.get({1,2,3}).r << '\n';
 return 0;
